57d91c9ab4d3e57ddd59445acde72d2e6b750f51
MishaRatnikov <none@none>
2019-05-03 20:47:27 +0300

    парсинг
    --HG--
    branch : article-design

---
+++ microservices/tools/modules/db/dynamic/models/donor_model.js
@@ -0,0 +1,9 @@
+var myModel = require('../../../../core/my_model');
+
+class Donor_model extends myModel{
+    constructor(table, config) {
+        super(table, config);
+    }
+}
+
+module.exports = Donor_model;
