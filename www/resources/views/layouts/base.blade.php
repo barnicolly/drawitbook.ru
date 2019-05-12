b2f26a854acb90a37209ff149dd40d087aba5560
MishaRatnikov <none@none>
2019-05-12 22:17:02 +0300

    добавление в вебмастера
    --HG--
    branch : production

--- www/resources/views/layouts/base.blade.php
+++ www/resources/views/layouts/base.blade.php
@@ -5,6 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="csrf-token" content="{{ csrf_token() }}"/>
+    <meta name="yandex-verification" content="e78a61097fbbb899" />
+    <meta name="google-site-verification" content="44qvqCqXAJ59PJZMKJB4zmk8zDa57Ff1mpau2pNfm3Q" />
     <link rel="icon" href="/favicon.ico">
     <title>{{ MetaTag::get('title') }}</title>
     @stack('head')
