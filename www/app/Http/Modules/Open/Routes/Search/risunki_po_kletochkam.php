ecea4d307a77a49378a8fd79180694bae4b26e28
MishaRatnikov <none@none>
2019-06-11 15:25:58 +0300

    пhавки
    --HG--
    branch : searchRisunkiPoKletochkam

--- www/app/Http/Modules/Open/Routes/Search/risunki_po_kletochkam.php
+++ www/app/Http/Modules/Open/Routes/Search/risunki_po_kletochkam.php
@@ -6,7 +6,7 @@ Route::group(
         'namespace' => 'App\Http\Modules\Open\Controllers\Search'
     ],
     function () {
-        Route::get('/search', ['uses' => 'Search@index'])
-            ->name('search');
+        Route::get('/risunki-po-kletochkam', ['uses' => 'RisunkiPoKletochkam@search'])->name('risunkiPoKletochkam.search');
+        Route::get('/search', ['uses' => 'Search@index'])->name('search');
         Route::get('/risunki-po-kletochkam/tagged/{tag}', ['uses' => 'RisunkiPoKletochkam@tagged'])->name('risunkiPoKletochkam.tagged');
     });
