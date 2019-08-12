65dd8a3a4f9d83db87ee4e46a919dbbfa4f88e10
MishaRatnikov <none@none>
2019-08-12 19:18:53 +0300

    удалены файлы ноды
    --HG--
    branch : production

--- microservices/tools/index.js
+++
@@ -1,5 +0,0 @@
-global.appConfig = require('./config/config');
-
-var core = require ('./modules/services/get_pict/controllers/index');
-
-core.init();
--- microservices/tools/modules/db/dynamic/models/donor_model.js
+++
@@ -1,9 +0,0 @@
-var myModel = require('../../../../core/my_model');
-
-class Donor_model extends myModel{
-    constructor(table, config) {
-        super(table, config);
-    }
-}
-
-module.exports = Donor_model;
