72f03ab18188e737bfa3a7749168cd494134ed14
MishaRatnikov <mik009@narod.ru>
2020-10-03 18:54:24 +0300

    Footer в БЭМ именование

--- www/resources/views/layouts/public/footer/base.blade.php
+++ www/resources/views/layouts/public/footer/base.blade.php
@@ -1,14 +1,14 @@
 <footer class="footer">
     <div class="footer__line"></div>
     <div class="container">
-        <div class="footer__slogan content">
+        <div class="footer__slogan text-uppercase content">
             Love art? We do too.
         </div>
     </div>
     <hr class="footer__delimiter">
     <div class="container">
         <div class="form-group content">
-            <span class="footer__app-name">DRAWITBOOK.RU</span> {{ date('Y') }} г. Приветствуется копирование и
+            <span class="footer__app-name text-uppercase">DRAWITBOOK.RU</span> {{ date('Y') }} г. Приветствуется копирование и
             распространение материалов.
         </div>
     </div>
