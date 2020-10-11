9219760a31a2c55b6a577ac93ffce6b9400f653f
MishaRatnikov <mik009@narod.ru>
2020-10-11 14:49:35 +0300

    Удален старый процесс добавления новых изображений

--- www/app/Http/Modules/Admin/Routes/web.php
+++ www/app/Http/Modules/Admin/Routes/web.php
@@ -3,20 +3,6 @@
 Route::group(
     [
         'middleware' => ['web', 'roles'],
-        'prefix' => '/admin',
-        'namespace' => 'App\Http\Modules\Admin\Controllers\Moderate',
-        'roles' => ['Admin']
-    ],
-    function () {
-        Route::get('/moderate', ['uses' => 'Index@index'])->name('moderate');
-        Route::post('/moderate/delete_image', ['uses' => 'DeleteImage@deleteImage']);
-        Route::post('/moderate/delete_images', ['uses' => 'DeleteImage@deleteImages']);
-        Route::post('/moderate/save_image', ['uses' => 'SaveImage@saveImage']);
-    });
-
-Route::group(
-    [
-        'middleware' => ['web', 'roles'],
         'prefix' => '/admin/art',
         'namespace' => 'App\Http\Modules\Admin\Controllers\Art',
         'roles' => ['Admin']
