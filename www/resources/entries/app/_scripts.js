32e8afd0c685cf2fc0a17d245ec19422f4b5f6ab
MishaRatnikov <mik009@narod.ru>
2020-10-11 17:17:40 +0300

    Убрал ненужные импорты в app

--- www/resources/entries/app/_scripts.js
+++ www/resources/entries/app/_scripts.js
@@ -1,7 +1,5 @@
 import '@js/components/stack_grid';
 import '@js/components/claim';
 import '@js/components/rate';
-//TODO-misha попытаться вынести из общего bundle;
-import '@js/components/art_control';
 
 import '@js/app.loader';
--- www/resources/entries/app/_vendors.js
+++ www/resources/entries/app/_vendors.js
@@ -4,7 +4,3 @@ import 'jquery';
 
 // import '@plugins/font-awesome/css/font-awesome.css';
 import '@plugins/font-awesome/dist/fa-purged.min.css';
-
-//TODO-misha убрать из vendor;
-import 'bootstrap/dist/js/bootstrap.bundle'
-import 'bootstrap/dist/css/bootstrap.min.css'
