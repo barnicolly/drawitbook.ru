5a0621d881fd71365ec42e1e180125fedaecf2bc
MishaRatnikov <mik009@narod.ru>
2020-10-22 07:37:44 +0300

    Не показывалась страница с 404

--- www/resources/views/layouts/errors.blade.php
+++ www/resources/views/layouts/errors.blade.php
@@ -1,16 +1,18 @@
 @extends('layouts.public.layout')
 
-@section('layout.body')
-    <div class="container errors-page">
+@section('layout.content')
+    <div class="errors-page">
         <div class="errors-page__img">
             <img class="img-responsive lazyload"
                  data-src="{{ buildUrl('img/page-not-found.png') }}" alt="Произошла ошибка">
         </div>
         <div class="errors-page__content">
-            <div class="form-group errors-page__title">
-                @yield('errors.title')
+            <div class="content">
+                <div class="form-group errors-page__title">
+                    @yield('errors.title')
+                </div>
+                @yield('errors.content')
             </div>
-            @yield('errors.content')
         </div>
     </div>
 @endsection
