b61af3db31e4d6c013da61a858e04758b53ce904
MishaRatnikov <mik009@narod.ru>
2021-02-16 14:32:26 +0300

    Изменил код верификации для яндекса

--- www/resources/views/layouts/partials/base/verification.blade.php
+++ www/resources/views/layouts/partials/base/verification.blade.php
@@ -1,2 +1,2 @@
-<meta name="yandex-verification" content="d8ea1d7bc86cefde" />
+<meta name="yandex-verification" content="c265d431ad2f5620" />
 <meta name="google-site-verification" content="VNU4DTs_VreeF6E_w_iYUmMitUhYJKyF__Yd5RE0BI0" />
