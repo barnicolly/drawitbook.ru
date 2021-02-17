4468d5398f42ec90296fe502b8adca1391d3f08d
MishaRatnikov <mik009@narod.ru>
2021-02-17 20:22:58 +0300

    Опечатка

--- www/resources/views/partials/breadcrumbs.blade.php
+++ www/resources/views/partials/breadcrumbs.blade.php
@@ -1,32 +1,29 @@
 @if (count($breadcrumbs))
-    <nav aria-label="breadcrumb" id="breadcrumb" itemscope="" itemtype="http://schema.org/BreadcrumbList">
+    <nav aria-label="breadcrumb" id="breadcrumb" itemscope itemtype="https://schema.org/BreadcrumbList">
         <ol class="breadcrumb">
             <meta name="numberOfItems" content="{{ count($breadcrumbs) }}">
             <meta name="itemListOrder" content="Ascending">
             @foreach ($breadcrumbs as $breadcrumb)
                 @if ($breadcrumb->url && !$loop->last)
-                    <li class="breadcrumb-item" itemscope="" itemprop="itemListElement"
-                        itemtype="http://schema.org/ListItem">
-                        <a itemprop="item" href="{{ $breadcrumb->url }}" title="{{ $breadcrumb->title }}">
+                    <li class="breadcrumb-item" itemprop="itemListElement" itemscope
+                        itemtype="https://schema.org/ListItem">
+                        <a itemprop="item" href="{{ $breadcrumb->url }}">
                             @if (isset($breadcrumb->icon))
                                 <span class="{{ $breadcrumb->icon }}"></span>
                                 <span itemprop="name">{{ $breadcrumb->title }}</span>
                             @else
                                 <span itemprop="name">{{ $breadcrumb->title }}</span>
                             @endif
-                            <meta itemprop="position" content="{{ ++$loop->index }}">
                         </a>
+                        <meta itemprop="position" content="{{ ++$loop->index }}"/>
                     </li>
                 @else
-                    <li class="breadcrumb-item active" itemscope="" itemprop="itemListElement"
-                        itemtype="http://schema.org/ListItem">
-                        <div itemprop="item">
-                            <span itemprop="name"> {{ $breadcrumb->title }}</span>
-                            <meta itemprop="position" content="{{ ++$loop->index }}">
-                        </div>
+                    <li class="breadcrumb-item active" itemprop="itemListElement" itemscope
+                        itemtype="https://schema.org/ListItem">
+                        <span itemprop="name"> {{ $breadcrumb->title }}</span>
+                        <meta itemprop="position" content="{{ ++$loop->index }}" />
                     </li>
                 @endif
-
             @endforeach
         </ol>
     </nav>
