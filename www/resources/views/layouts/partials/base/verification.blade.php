1d45da251b8511c7455a691bcdd07c82c063020d
MishaRatnikov <mik009@narod.ru>
2021-02-17 11:53:27 +0300

    Добавлен код верификации для mail.ru

--- www/resources/views/layouts/partials/base/verification.blade.php
+++ www/resources/views/layouts/partials/base/verification.blade.php
@@ -1,2 +1,3 @@
 <meta name="yandex-verification" content="c265d431ad2f5620" />
 <meta name="google-site-verification" content="VNU4DTs_VreeF6E_w_iYUmMitUhYJKyF__Yd5RE0BI0" />
+<meta name='wmail-verification' content='3a8b8de38084763ee082d8a3da866c8f' />
