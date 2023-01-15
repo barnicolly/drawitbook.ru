c82a2a91c59937cee37f8f18a8169dbe14f4c1f3
MishaRatnikov <mik009@yandex.ru>
2023-01-15 18:42:07 +0300

    refactor: Псевдоним для полиморфических связей
    https://laravel.com/docs/9.x/eloquent-relationships#custom-polymorphic-types

--- www/app/Ship/Providers/AppServiceProvider.php
+++ www/app/Ship/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Ship\Providers;
 
+use App\Containers\Picture\Models\PictureModel;
+use App\Containers\Tag\Models\SprTagsModel;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
@@ -26,6 +29,14 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Model::shouldBeStrict(!$this->app->isProduction());
+
+        Relation::enforceMorphMap(
+            [
+                'tag' => SprTagsModel::class,
+                'art' => PictureModel::class,
+            ]
+        );
+
         if (config('app.forceHttps') === true) {
             URL::forceScheme('https');
         }
