fcb123c7f380ec606aa7857521e4823e8d8024a4
MishaRatnikov <mik009@yandex.ru>
2023-04-17 08:31:31 +0300

    feat: Подключен laravel dusk

--- www/webpack/dev.conf.js
+++ www/webpack/dev.conf.js
@@ -2,4 +2,12 @@ const {merge} = require('webpack-merge');
 const baseWebpackConfig = require('./base.conf');
 const buildWebpackConfig = require('@misharatnikov/webpack/src/dev.conf');
 
-module.exports = merge(buildWebpackConfig, baseWebpackConfig);
+const domain = 'http://172.17.0.1';
+const port = process.env.WEBPACK_DEVSERVER_IN_PORT;
+const devWebpackConfig = merge(buildWebpackConfig, baseWebpackConfig, {
+    output: {
+        publicPath: `${domain}:${port}/build/`,
+    },
+});
+
+module.exports = devWebpackConfig;
