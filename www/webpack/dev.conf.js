81efbd7a8461072deeeb88fc0b8431d237d7d17b
MishaRatnikov <mik009@yandex.ru>
2023-05-03 08:43:13 +0300

    refactor: Соединение с webpack через nginx proxy

--- www/webpack/dev.conf.js
+++ www/webpack/dev.conf.js
@@ -3,11 +3,15 @@ const baseWebpackConfig = require('./base.conf');
 const buildWebpackConfig = require('@misharatnikov/webpack/src/dev.conf');
 
 const domain = 'http://172.17.0.1';
-const port = process.env.WEBPACK_DEVSERVER_IN_PORT;
+const port = 8111;
 const devWebpackConfig = merge(buildWebpackConfig, baseWebpackConfig, {
     output: {
-        publicPath: `${domain}:${port}/build/`,
+        publicPath: `/build/`,
     },
+    devServer: {
+        port: 3000,
+        sockPort: 'location',
+    }
 });
 
 module.exports = devWebpackConfig;
